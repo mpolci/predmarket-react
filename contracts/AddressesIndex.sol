0406f86034d8840000bc165f9897c9e9300a2602
Marco <mpolci@gmail.com>
2016-07-20 23:00:58 +0200

    Add AddressesIndex contract

---
+++ contracts/AddressesIndex.sol
@@ -0,0 +1,15 @@
+contract AddressesIndex {
+  mapping (address => address[]) index;
+
+  function add(address toAdd) {
+    index[msg.sender].push(toAdd);
+  }
+
+  function get() constant returns (address[]) {
+    return index[msg.sender];
+  }
+
+  function remove(uint idx) {
+    delete index[msg.sender][idx];
+  }
+}
--- truffle.js
+++ truffle.js
@@ -10,7 +10,7 @@ module.exports = {
     "images/": "images/"
   },
   deploy: [
-
+    "AddressesIndex"
   ],
   rpc: {
     host: "localhost",
