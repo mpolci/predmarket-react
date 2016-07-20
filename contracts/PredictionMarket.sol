b495781542a2a4139b23c9f7bd029fde2a0db59a
Marco <mpolci@gmail.com>
2016-07-20 19:21:09 +0200

    Add prediction market description

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -1,3 +1,23 @@
+/*
+  Prediction market beavior:
+  - the account that creates the contract becomes the contract owner
+  - the contract creation should transfer at least 1 finney, and the transfered value is the initial prize
+  - the contract creation should specify the question, the expiration time, the address of the responder account and the fee rate
+  - the contract creation assign to the owner a number of bids for both yes and no replies, the number of bids is equal to n = trasfered value / 0.5 finney (n bids for yes and n for no)
+  - the price for a bid is calculated as 1 finney * x / total bids, where x is the number of previous bids for current bid type (yes or no), and total bids is the total number of both bids types
+  - the bids can be buyed before the expiration time
+  - the responder account can give a verdict after the expiration time within a week
+  - after the responder has given the verdict, the winners users can withdraw the prize (also the owner have some bids for the initial prize)
+  - after the responder has given the verdict, the owner can withdraw the fees
+  - the prize for each bids is calculated as (contract balance - fees) / number of winning bids
+  - the fees are calculated at the specified fee rate of the contract balance
+  - if the responder doesn't give the verdict within a week, the contract balance is divided among all bids excluded the owner's bids for initial prize. The owner loses anything
+  - after the expiration time plus 4 weeks the prediction market is over, the owner can destroy it and take all the remaining balance that was not collected by bidders
+
+  Possible improvements:
+  - payment of the responder
+  - parameterize the constants
+*/
 import 'AnswerToken.sol';
 
 contract PredictionMarket {
@@ -12,6 +32,7 @@ contract PredictionMarket {
   uint public payout;
   uint public feeRate;  // value 100 means 1%
 
+  // _feeRate is calculated as 1/10000 of total value, so a value of 100 means 1%
   function PredictionMarket(string _question, uint _expirationTime, address _responder, uint _feeRate) {
     if (_expirationTime <= now) throw;
     if (_responder == 0) throw;
